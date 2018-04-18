6944de266232a5a54d12179737f87ca4b23b0d53
Vincent Luczkow <vincent.luczkow@gmail.com>
2018-04-18 01:50:38 -0400

    Figured out how to mimic 1d locally connected with 2d locally connected

--- test/local_conv_test.py
+++ test/local_conv_test.py
@@ -11,7 +11,7 @@ import pytest
 
 
 def test_first():
-    a = locally_connected.Conv2dLocal(in_channels=256, out_channels=256, in_height=8, in_width=8, kernel_size=3, stride=1, padding=0)
+    a = locally_connected.Conv2dLocal(in_channels=256,  out_channels=256, in_height=8, in_width=8, kernel_size=3, stride=1, padding=0)
     input = Variable(torch.randn(1, 256, 8, 8))
     output = a(input)
 
@@ -20,7 +20,7 @@ def test_dense_network():
     pass
 
 
-def test_flattened_2d_local():
+def test_flattened_keras():
     input_height = 32
     input_width = 1
     in_channels = 10
@@ -29,8 +29,36 @@ def test_flattened_2d_local():
     filters = 9
     stride_x = 1
     stride_y = 1
+    input1_shape = (input_height, in_channels)
+    input2_shape = (input_height, 1, in_channels)
+    model1 = keras.Sequential()
+    model1.add(keras.layers.LocallyConnected1D(filters, kernel_size, input_shape=input1_shape))
+    model2 = keras.Sequential()
+    model2.add(keras.layers.LocallyConnected2D(filters, (kernel_size, 1), input_shape=input2_shape))
+    x = np.zeros((1, input_height, in_channels), dtype=np.float32)
+    x2 = x.reshape([1, input_height, 1, in_channels])
+    first_pred = model1.predict(x)
+    second_pred = model2.predict(x2)
+    ipdb.set_trace()
+    # var = Variable(torch.from_numpy(x)).cuda()
+    # k_res = model.predict(x)
+
+
+def test_flattened_2d_local():
+    input_height = 32
+    output_height = 30
+    input_width = 1
+    in_channels = 10
+    kernel_size = 3
+    kernel_width = 1
+    filters = 9
+    stride_x = 1
+    stride_y = 1
+    input1_shape = (input_height, in_channels)
+    input2_shape = (input_height, 1, in_channels)
+
     model = keras.Sequential()
-    model.add(keras.layers.LocallyConnected1D(filters, kernel_size, input_shape=(in_channels, input_height), data_format="channels_first"))
+    model.add(keras.layers.LocallyConnected1D(filters, kernel_size, input_shape=input1_shape))
     pt = locally_connected.Conv2dLocal(
         in_height=input_height,
         in_width=1,
@@ -41,18 +69,20 @@ def test_flattened_2d_local():
         bias=False
     ).cuda()
 
-    x = np.zeros((4, in_channels, input_height, 1), dtype=np.float32)
-    var = Variable(torch.from_numpy(x)).cuda()
-    k_res = model.predict(x[:, :, :, 0])
-    t_res = pt(var).cpu().data.numpy()
+    x = np.zeros(input1_shape, dtype=np.float32)
+    input1 = x.reshape((1, input_height, in_channels))
+    input2 = x.transpose().reshape((1, in_channels, input_height, 1))
+    var = Variable(torch.from_numpy(input2)).cuda()
+    k_res = model.predict(input1).reshape((output_height, filters))
+    t_res = pt(var).cpu().data.numpy().reshape((filters, output_height)).transpose()
+    ipdb.set_trace()
     assert (k_res == t_res).all()
 
 
+# TODO: Copy weights between models.
 @pytest.mark.skip()
 def test_compare_2d_local():
-
-    for i in range(10):
-
+    for i in range(1):
         input_height = np.random.randint(5, 64)
         input_width = np.random.randint(5, 64)
         in_channels = np.random.randint(1, 8)
@@ -62,7 +92,8 @@ def test_compare_2d_local():
         stride_x = 1
         stride_y = 1
         model = keras.Sequential()
-        model.add(keras.layers.LocallyConnected2D(filters, (kernel_height, kernel_width), input_shape=(in_channels, input_height, input_width), data_format="channels_first"))
+        model.add(keras.layers.LocallyConnected2D(filters, (kernel_height, kernel_width), use_bias=False, input_shape=(in_channels, input_height, input_width),
+                                                  data_format="channels_first"))
         pt = locally_connected.Conv2dLocal(
             in_height=input_height,
             in_width=input_width,
